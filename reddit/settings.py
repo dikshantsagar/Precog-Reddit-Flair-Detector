db8d5b551852d10c9bb650f5f65228196d7b73c3
Dikshant Sagar <sagar.dikshant@gmail.com>
2019-07-18 23:46:40 +0530

    Update settings.py

--- reddit/settings.py
+++ reddit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zk#w%@5x_x5mcd21vs#06nc*2b^1lg@9mxiy3t(=z$)3z642bx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['redditflairdetector.herokuapp.com']
 
