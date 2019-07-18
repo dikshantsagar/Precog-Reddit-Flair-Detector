fb9627dcbbc9f51fed0e07445a94de995c9d17b1
Dikshant Sagar <sagar.dikshant@gmail.com>
2019-07-18 23:17:29 +0530

    Update settings.py

--- reddit/settings.py
+++ reddit/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = os.path.join(PROJECT_DIR,'static/')
+STATIC_URL = os.path.join('static')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, "static"),
 
