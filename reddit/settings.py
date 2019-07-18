c502f3ce10ad43e5464296004f4f4073e13b5f76
Dikshant Sagar <sagar.dikshant@gmail.com>
2019-07-18 23:42:55 +0530

    Update settings.py

--- reddit/settings.py
+++ reddit/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'static'
-STATICFILES_DIRS = [("static", 'static')]
-STATIC_ROOT='static'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/']
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 os.makedirs(STATIC_ROOT, exist_ok=True)
